1c663e1f85c96a0c910f3ac7bd2d919c6436d119
bishwas <bishwas.niraula@gmail.com>
2019-06-03 16:32:10 -0500

    puhsing results

--- assignment5/js/ascii.js
+++ assignment5/js/ascii.js
@@ -1,15 +1,35 @@
 var start=document.getElementById("start");
 var stop=document.getElementById("stop");
 var animation =document.getElementById("animation");
+var size=document.getElementById("size");
 start.onclick=function(){
+    startAnimation();
     var whichOne = "Juggler";
     document.getElementById("animation-screen-id").value = ANIMATIONS[whichOne];
 }
 stop.onclick=function(){
+    stopAnimation();
     var whichOne = "Blank";
     document.getElementById("animation-screen-id").value = ANIMATIONS[whichOne];
 }
 animation.onchange=function(){
     var whichOne=this.value;
     document.getElementById("animation-screen-id").value = ANIMATIONS[whichOne];
+}
+size.onchange=function(){
+    var whichOne=this.value;
+    document.getElementById("animation-screen-id").style.fontSize=whichOne+"px";
+
+}
+
+function startAnimation(){
+    start.disabled=true;
+    stop.disabled=false;
+    animation.disabled=true;
+
+}
+function stopAnimation(){
+    start.disabled=false;
+    stop.disabled=true;
+    animation.disabled=false;
 }

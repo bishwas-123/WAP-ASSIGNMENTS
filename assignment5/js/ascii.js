7b6efe4f6153d278d1af1084b4e8d72edc61d281
bishwas <bishwas.niraula@gmail.com>
2019-06-04 21:55:22 -0500

    pushing changes

--- assignment5/js/ascii.js
+++ assignment5/js/ascii.js
@@ -1,51 +1,74 @@
-var start=document.getElementById("start");
-var stop=document.getElementById("stop");
-var animation =document.getElementById("animation");
-var size=document.getElementById("size");
-start.onclick=function(){
-    startAnimation();
-    var whichOne = "Juggler";
-    document.getElementById("animation-screen-id").value = ANIMATIONS[whichOne];
-}
-stop.onclick=function(){
-    stopAnimation();
-    var whichOne = "Blank";
-    document.getElementById("animation-screen-id").value = ANIMATIONS[whichOne];
-}
-animation.onchange=function(){
-    var whichOne=this.value;
-    document.getElementById("animation-screen-id").value = ANIMATIONS[whichOne];
-}
-size.onchange=function(){
-    var whichOne=this.value;
-    document.getElementById("animation-screen-id").style.fontSize=whichOne+"px";
-
-}
-
-function repeatAnimation(){
-    var val=ocument.getElementById("animation-screen-id").value;
-}
-
-function startAnimation(){
-    start.disabled=true;
-    stop.disabled=false;
-    animation.disabled=true;
-
+(function(){
+    "use strict"
+    var start=document.getElementById("start");
+    var stop=document.getElementById("stop");
+    var size=document.getElementById("size");
+     var screen =document.getElementById("animation-screen-id");
+    var timer;
+    var index=0;
+    var turboStatus=250;
+    start.onclick=startAnimation;
     
-    //repeatAnimation();
-    // var timer = setInterval(frame, 10);
-    // function frame() {
-    //   if (pos == 350) {
-    //     clearInterval(id);
-    //   } else {
-    //     pos++; 
-    //     elem.style.top = pos + 'px'; 
-    //     elem.style.left = pos + 'px'; 
-    //   }
+    function startAnimation(){
+        var whichOne=document.getElementById("animation");
+        var whichOne = whichOne.options[whichOne.selectedIndex].value;
+        if(whichOne){
+            toggleEvent();
+            timer=setInterval(
+                function(){
+                startTimer(ANIMATIONS[whichOne]);},turboStatus
+            );
+        }
+   
+    }
+    document.getElementById("turbo").onchange=function(){
+        clearInterval(timer);
+        if(document.getElementById("turbo").checked==false){
+            turboStatus=250;
+        }
+        else{
+            turboStatus=50;
+        }
+        startAnimation();
+    }
+    function startTimer(val) {
+        var str=val.split("=====\n");
+         if(index==str.length){
+             index=0;
+         }
+         var x = str[index];
+         screen.value=x;
+         index++;
+      }
+    stop.onclick=function(){
+        clearInterval(timer);
+        stopAnimation();
+        // var whichOne = "Blank";
+        // document.getElementById("animation-screen-id").value = ANIMATIONS[whichOne];
+    }
+    size.onchange=function(){
+        var whichOne=this.value;
+        document.getElementById("animation-screen-id").style.fontSize=whichOne+"px";
+    
+    }
+    
+    
+    
+    function toggleEvent(){
+        start.disabled=true;
+        stop.disabled=false;
+        animation.disabled=true;
+    
+    
+    
+    }
+    function stopAnimation(){
+     
+        start.disabled=false;
+        stop.disabled=true;
+        animation.disabled=false;
+    }
 
-}
-function stopAnimation(){
-    start.disabled=false;
-    stop.disabled=true;
-    animation.disabled=false;
-}
+})();
+
+

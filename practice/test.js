0734a4fa51f4effef35c08544d0dadaa53ace7a6
bishwas <bishwas.niraula@gmail.com>
2019-06-11 16:28:31 -0500

    hhhhhhhhhhhhhhhhhhhhh

--- practice/test.js
+++ practice/test.js
@@ -7,6 +7,38 @@
 // bar2();
 // obj.foo = foo;
 // obj.foo();
+function calculate(f, a, b) { return f(a, b); }
+var op = function(x,y){return x+y;};
+var op1 = function (x,y){return x*y;};
+alert(calculate(op1,3,4));
+alert(calculate(op,9,6));
+
+var myfunc = function(a, x) {
+return a * x;
+};
+var x = myfunc(2, 3);
+var y = myfunc;
+//alert(x);
+//alert(y(2,3));
+
+
+
+setTimeout(donkey1(), 12000);
+setTimeout(donkey2, 12000);
+
+function donkey2(){
+	alert("d1");
+}
+
+function donkey1(){
+	
+	return function(){
+		
+		alert("d2");
+	}
+	
+}
+
 
 var a = {
     greet: '',

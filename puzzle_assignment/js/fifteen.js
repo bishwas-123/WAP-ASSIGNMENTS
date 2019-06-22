e2905cad0ebbcd60de6a0f8cba0886e528df077e
bishwas-123 <bishwash.niraula@gmail.com>
2019-06-22 15:07:27 -0500

    Update fifteen.js

--- puzzle_assignment/js/fifteen.js
+++ puzzle_assignment/js/fifteen.js
@@ -4,33 +4,30 @@ $(document).ready(function () {
     let currentOrder = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, null];
 
 
-init = function() {
-    var puzzleArea = document.getElementById('puzzlearea');
-    var divs = puzzleArea.getElementsByTagName("div");
-      
-    // initialize each piece
-    for (var i=0; i< divs.length; i++) {
-        var div = divs[i];
-        
-        // calculate x and y for this piece
-        var x = ((i % 4) * 100) ;
-        var y = (Math.floor(i / 4) * 100) ;
-
-        // set basic style and background
-        div.className = "puzzlepiece";
-        div.style.left = x + 'px';
-        div.style.top = y + 'px';
-        div.style.backgroundImage = 'url("images/background.jpg")';
-        div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
-        
-        // store x and y for later
-        div.x = x;
-        div.y = y; 
-    }        
-};
-
+    init = function () {
+        // let puzzleArea = document.getElementById('puzzlearea');
+        let divs = $('#puzzlearea div');
+        // let divs = puzzleArea.getElementsByTagName("div");
+        // initialize each piece
+        for (var i = 0; i < divs.length; i++) {
+            var div = divs[i];
+            // calculate x and y for this piece
+            var x = ((i % 4) * 100);
+            var y = (Math.floor(i / 4) * 100);
+            // set basic style and background
+            div.className = "puzzlepiece";
+            div.id = i;
+            div.style.left = x + 'px';
+            div.style.top = y + 'px';
+                   div.style.backgroundImage = 'url("images/background.jpg")';
+            div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
+            // store x and y for later
+            div.x = x;
+            div.y = y;
+        }
+    };
     init();
-//  random order
+// shuffles all the pieces in a random order
     $("#shufflebutton").click(function () {
         let divs = $(".puzzlepiece");
         var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
@@ -68,54 +65,54 @@ init = function() {
             }
         }
     });
-
+// moves the piece as soon is clicked
     $("#puzzlearea div").click(function () {
-        let space = currentOrder.indexOf(null);
+        let blankspace = currentOrder.indexOf(null);
         let nearbyPieces = [null, null, null, null];
         let index = 0;
-        if (space > 3) {
-            let up = space - 4;
+        if (blankspace > 3) {
+            let up = blankspace - 4;
             nearbyPieces[0] = currentOrder[up];
         }
-        if ((space % 4) >= 0 && (space % 4) < 3) {
-            let right = space + 1;
+        if ((blankspace % 4) >= 0 && (blankspace % 4) < 3) {
+            let right = blankspace + 1;
             nearbyPieces[1] = currentOrder[right];
         }
-        if (space < 12) {
-            let down = space + 4;
+        if (blankspace < 12) {
+            let down = blankspace + 4;
             nearbyPieces[2] = currentOrder[down];
         }
-        if ((space % 4) <= 3 && (space % 4) > 0) {
-            let left = space - 1;
+        if ((blankspace % 4) <= 3 && (blankspace % 4) > 0) {
+            let left = blankspace - 1;
             nearbyPieces[3] = currentOrder[left];
         }
         let divId = parseInt(this.id);
         //verifies if the piece can be moved
         if (nearbyPieces.includes(divId)) {
             let PiecePosition = nearbyPieces.indexOf(divId);
-            currentOrder[space] = divId;
+            currentOrder[blankspace] = divId;
             let y = parseInt(this.style.top);
             let x = parseInt(this.style.left);
             let newY = 0;
             let newX = 0;
             switch (PiecePosition) {
                 case 0:
-                    currentOrder[space - 4] = null;
+                    currentOrder[blankspace - 4] = null;
                     newY = y + 100;
                     this.style.top = newY + 'px';
                     break;
                 case 1:
-                    currentOrder[space + 1] = null;
+                    currentOrder[blankspace + 1] = null;
                     newX = x - 100;
                     this.style.left = newX + 'px';
                     break;
                 case 2:
-                    currentOrder[space + 4] = null;
+                    currentOrder[blankspace + 4] = null;
                     newY = y - 100;
                     this.style.top = newY + 'px';
                     break;
                 case 3:
-                    currentOrder[space - 1] = null;
+                    currentOrder[blankspace - 1] = null;
                     newX = x + 100;
                     this.style.left = newX + 'px';
                     break;
